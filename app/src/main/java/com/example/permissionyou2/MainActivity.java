aecb5d58419dd7e08b88e364db5f8c4f2c841c9e
nalin <nalinikanta7978@gmail.com>
2021-07-07 07:51:28 +0530

    c1

---
+++ app/src/main/java/com/example/permissionyou2/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.permissionyou2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private final int REAQUEST_CODE=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[]permission={Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                ActivityCompat.requestPermissions(MainActivity.this,permission,REAQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0]== PackageManager.PERMISSION_GRANTED&&grantResults[1]==PackageManager.PERMISSION_GRANTED){
+            showToast("Storage and Location permission granted");
+        }else if (grantResults[0]==PackageManager.PERMISSION_GRANTED&&grantResults[1]==PackageManager.PERMISSION_DENIED){
+            showToast("Storage denied but Location permission granted");
+        }else if (grantResults[0]==PackageManager.PERMISSION_DENIED&&grantResults[1]==PackageManager.PERMISSION_GRANTED){
+            showToast("Location denied but Storage permission granted ");
+        }else if (grantResults[0]==PackageManager.PERMISSION_DENIED&&grantResults[1]==PackageManager.PERMISSION_DENIED){
+            showToast("Both the permission denied");
+        }
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+}
